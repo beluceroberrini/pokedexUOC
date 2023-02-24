9ac9251a7d56ac46e1fb3dd49ca5124f4699248c
Maria Belen <belul@hotmail.com>
2023-02-24 12:43:27 +0100

    Home

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,12 @@
                 <img
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png">
             </router-link> 
+
+<!-- 
+        <section class="moon">
+            <img src="./imagenes/moon-solid.svg" id="icon" class='icon' alt="">
+            <button class="myButton"></button>
+        </section> -->
         </div>
     </nav>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,31 @@
 <template>
-    
+    <div class="welcome">
+        <h1 class="title">Bienvenido al mundo Pokemon</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, laudantium eaque mollitia dolore numquam sed? Rem optio quaerat adipisci error.</p>
+        <h2 class="title">GO!</h2>
+    </div>
 </template>
 <script setup>
 </script>
 <style scoped>
+
+.welcome{
+    display: flex;
+    justify-content:center;
+    flex-direction: column; 
+    align-items: center;
+    padding-top: 120px;
+}
+.title {
+    margin: 20px;
+    font-size: 50px;
+}
+
+h2{
+    margin: 50px;
+    font-size: 50px;
+}
+p{
+    width: 45%;
+}
 </style>
